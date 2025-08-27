4bc023d8bc71b3652819b4a8b9b6bc7bdeda7746
Nkwi Cyril <akinimbomnkwi@gmail.com>
2025-08-27 09:14:00 +0100

    Updated logic for tip calculator

--- day_2/tip_calculator.py
+++ day_2/tip_calculator.py
@@ -1,11 +1,16 @@
 print("Welcome to the TIP Calculator")
 
 bill = float(input("What is the total bill? $"))
-tip_percentage = int(input("How much tip would you like to give? 10, 12 or 15? "))
+tip = int(input("How much tip would you like to give? 10, 12 or 15? "))
 num_people = int(input("How many people to split the bill? "))
 
-def calculate_tip(bill, tip_percentage, num_people):
-    tip = (bill * (tip_percentage/100)) / num_people
-    return tip
+def calculate_tip(bill, tip, num_people):
+    tip_percentage = tip / 100
+    total_tip_amount = bill * tip_percentage
+    total_bill = bill + total_tip_amount
 
-print(f"Each person should pay: ${calculate_tip(bill, tip_percentage, num_people)}")
+    shared_bill = total_bill / num_people
+
+    return shared_bill
+
+print(f"Each person should pay: ${calculate_tip(bill, tip, num_people)}")
